f2a8aa763dd2c52a1db0eaaaa9367a5da68235fc
purellarohith <purellarohith@gmail.com>
2020-10-28 01:18:19 +0530

    buddy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <p>This is Rohith</p>
+       <p>This is Rohith Purella Using Netlify and Buddy.Work for React app to go <b>ONLINE</b> </p>
       </header>
     </div>
   );
