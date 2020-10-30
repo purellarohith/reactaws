4b815f0d7091b796616296e1b8da5557c8814899
purellarohith <purellarohith@gmail.com>
2020-10-30 14:43:04 +0530

    add google search meta tags

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ const App = () => {
     <Helmet>
       <title>Home Page</title>
       <meta name='description' content='this is first home page in react app from rohith' />
-      <meta name="google-site-verification" content="bb-bvAkjTtNd1G6aQ5y6OalW-190YSpc59UwEaujCRc" />
     </Helmet>
       <header className="App-header">
        <p>This is Rohith Purella Using Netlify and Buddy.Work for React app to go <b>ONLINE</b> </p>
