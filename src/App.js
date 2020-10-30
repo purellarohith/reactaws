1c31832b637067b8d77b828637c8e081b798ff4a
purellarohith <purellarohith@gmail.com>
2020-10-30 14:35:30 +0530

    added helmet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+// Liberaries
+import React from 'react'
+//installed Liberaries
+import {Helmet} from 'react-helmet'
+//Styles
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
+    <Helmet>
+      <title>Home Page</title>
+      <meta name='description' content='this is first home page in react app from rohith' />
+    </Helmet>
       <header className="App-header">
        <p>This is Rohith Purella Using Netlify and Buddy.Work for React app to go <b>ONLINE</b> </p>
       </header>
