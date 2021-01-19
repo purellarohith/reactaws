94277743f9c9010e650d9aca1b06a0dca28702f6
rohit1436 <rohithpatel54@gmail.com>
2021-01-19 08:14:32 +0000

    add tags

--- src/Routes/ScreenOne.js
+++ src/Routes/ScreenOne.js
@@ -7,8 +7,10 @@ const ScreenOne = () => {
   return(
     <div className={'mainDiv'}>
     <Helmet>
-      <title>Home Page</title>
+      <title>Splash Screen</title>
       <meta name='description' content='this is first home page in react app from rohith' />
+      <meta name="author" content="Rohith Purella" />
+      <meta name="keywords" content="HTML, CSS, JavaScript,Coddevpro" />
     </Helmet>
    <header>
     <h1 className={'mainTitle'}>
